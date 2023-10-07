8c3d11e492545066a298d351cf510f0b7405c7d0
hp <hp@DESKTOP-RKDAC7S>
2023-10-07 22:14:32 +0530

    Third commit ----

--- src/Test1.java
+++ src/Test1.java
@@ -3,6 +3,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		String hungry = "yes";
 		System.out.println("HII");
+		System.out.println("welcome to nishant");
 		if (hungry == "yes")
 			System.out.println("eat something.....");
 		else {
