a50823b8c98299310ed515f16ff9d83702ebd230
hp <hp@DESKTOP-RKDAC7S>
2023-10-07 22:08:52 +0530

    second commit

--- src/Test1.java
+++ src/Test1.java
@@ -2,6 +2,7 @@
 public class Test1 {
 	public static void main(String[] args) {
 		String hungry = "yes";
+		System.out.println("HII");
 		if (hungry == "yes")
 			System.out.println("eat something.....");
 		else {
